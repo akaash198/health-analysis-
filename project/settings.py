4984e358214e75713ac294b8dbcd207bcb4e97e7
Akaash C <68701225+akaash198@users.noreply.github.com>
2022-02-15 19:22:56 +0530

    Add files via upload

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
